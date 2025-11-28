ba943a5b8f289ec184db00c4d98900553724496e
muhammadrayhanwidayat <murayat99@gmail.com>
2025-11-28 10:03:07 +0700

    siswa dao

---
+++ app/src/main/java/com/example/projectksver/room/SiswaDao.kt
@@ -0,0 +1,18 @@
+package com.example.projectksver.room
+
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+
+    @Query(value = "SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+}
