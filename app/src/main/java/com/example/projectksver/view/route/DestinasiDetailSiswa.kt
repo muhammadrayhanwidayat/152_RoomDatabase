7d08cc1684a79a22c58af24d789189fdf211cb55
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-05 09:59:55 +0700

    perbaikan

--- app/src/main/java/com/example/projectksver/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/projectksver/view/route/DestinasiDetailSiswa.kt
@@ -1,2 +1,10 @@
 package com.example.projectksver.view.route
 
+import com.example.projectksver.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
