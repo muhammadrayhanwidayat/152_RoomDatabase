b18a3d30d1fd0144acb65fffb1199e8df7c2dd4f
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-05 09:14:48 +0700

    change error

--- app/src/main/java/com/example/projectksver/MainActivity.kt
+++ app/src/main/java/com/example/projectksver/MainActivity.kt
@@ -30,26 +30,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjectksverTheme {
-        Greeting("Android")
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjectksverTheme {
-        Greeting("Android")
-    }
-}
