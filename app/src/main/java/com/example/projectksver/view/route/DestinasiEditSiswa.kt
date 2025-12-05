f3ed97e52fa007ec4bab96e98a8693263602a790
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-05 10:16:37 +0700

    update edit siswa

---
+++ app/src/main/java/com/example/projectksver/view/route/DestinasiEditSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.projectksver.view.route
+
+import com.example.projectksver.R
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
