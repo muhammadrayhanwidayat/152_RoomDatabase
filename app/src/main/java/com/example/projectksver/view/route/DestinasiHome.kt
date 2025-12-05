88ca50996b053d1cd369c391a80589778ea1cb32
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-05 08:58:21 +0700

    perbaiki import

---
+++ app/src/main/java/com/example/projectksver/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.projectksver.view.route
+
+import com.example.projectksver.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: Int = R.string.app_name
+}
