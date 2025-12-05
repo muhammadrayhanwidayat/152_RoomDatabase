9092e7d2815009475677e258ae76cdb1bcc0a4d6
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-05 10:13:52 +0700

    update siswa dao

--- app/src/main/java/com/example/projectksver/room/SiswaDao.kt
+++ app/src/main/java/com/example/projectksver/room/SiswaDao.kt
@@ -5,6 +5,8 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Delete
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -15,4 +17,14 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    //Edit : Tambah 3 Fungsi berikut
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
+    @Update
+    suspend fun update(siswa: Siswa)
 }
